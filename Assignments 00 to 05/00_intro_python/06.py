a6a1053aeaf1ab52f98910d61f535c84409bcfff
SHAH MIR USMAN <shahmirusman200@gmail.com>
2025-03-22 01:08:38 +0500

    Delete Assignments 00 to 05/00_intro_python directory

--- Assignments 00 to 05/00_intro_python/06.py
+++
@@ -1,9 +0,0 @@
-def squ():
-
-    print("Welcome to Square Calculator!")
-
-    num = int(input("Enter a number: "))
-    print(f"The square of the given number is: {num ** 2}.")
-    print("Thank you, Habibi!")
-
-squ()
