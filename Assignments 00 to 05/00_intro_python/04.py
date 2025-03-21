c7e0c22ad9a9fb9b6e318cac52f567c8362b68cb
SHAH MIR USMAN <shahmirusman200@gmail.com>
2025-03-22 01:11:18 +0500

    Python

---
+++ Assignments 00 to 05/00_intro_python/04.py
@@ -0,0 +1,16 @@
+def age():
+
+    Anton:int=21
+    Beth:int=6+Anton
+    Chen:int=21+Beth
+    Drew:int=Chen+Anton
+    Ethan:int=Chen
+
+    print("Anton is",Anton,"Years Old.")
+    print("Beth is",Beth,"Years Old.")
+    print("Chen is",Chen,"Years Old.")
+    print("Drew is",Drew,"Years Old.")
+    print("Ethan is",Ethan,"Years Old.")
+
+age()
+
---
+++ Assignments 00 to 05/00_intro_python/05.py
@@ -0,0 +1,14 @@
+def parameter_len():
+
+    print("Welcome to Python Perimeter Calculator!")
+
+    side1 = float(input("Enter length of first side: "))
+    side2 = float(input("Enter length of second side: "))
+    side3 = float(input("Enter length of third side: "))
+
+    perimeter = side1 + side2 + side3
+
+    print(f"The perimeter length is: {perimeter}.")
+    print("Thank you Habibi!")
+
+parameter_len()
